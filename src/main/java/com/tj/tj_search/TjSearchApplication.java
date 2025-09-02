b6733db58c9fcb3ec6afb191d95209d50af04082
PezKid <pezmanmc@gmail.com>
2025-09-02 16:28:46 -0700

    Initialize Spring Boot project setup.

---
+++ src/main/java/com/tj/tj_search/TjSearchApplication.java
@@ -0,0 +1,13 @@
+package com.tj.tj_search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tj/tj_search/TjSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tj.tj_search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
